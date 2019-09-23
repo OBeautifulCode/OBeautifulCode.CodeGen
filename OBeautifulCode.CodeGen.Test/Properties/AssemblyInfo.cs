7ee509b299478606e362f27a5189efa6ac5e3a57
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-09-23 13:09:56 -0400

    seed the solution

---
+++ OBeautifulCode.CodeGen.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OBeautifulCode.CodeGen.Test")]
+[assembly: AssemblyDescription("OBeautifulCode.CodeGen.Test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OBeautifulCode")]
+[assembly: AssemblyProduct("OBeautifulCode.CodeGen.Test")]
+[assembly: AssemblyCopyright("Copyright (c) 2019 OBeautifulCode")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("930983bd-5103-4991-836e-ddd80472a153")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # OBeautifulCode.CodeGen
+Code generation library to automate the creation of boilerplate code.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/0geiyokmim1jrvr7?svg=true)](https://ci.appveyor.com/project/SurajGupta/obeautifulcode-codegen)
+
+[![Nuget status](https://img.shields.io/nuget/v/OBeautifulCode.CodeGen.svg)](https://www.nuget.org/packages/OBeautifulCode.CodeGen)  OBeautifulCode.CodeGen
