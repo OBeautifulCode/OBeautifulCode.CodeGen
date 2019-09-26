9b9f428b6a6299c54d19e9530ad47169ea4a07e1
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-09-25 20:36:30 -0400

    more hardening

--- OBeautifulCode.CodeGen.ModelObject/Generation/GenerationShared.cs
+++ OBeautifulCode.CodeGen.ModelObject/Generation/GenerationShared.cs
@@ -55,5 +55,31 @@ namespace OBeautifulCode.CodeGen.ModelObject
 
             return result;
         }
+
+        /// <summary>
+        /// Gets the Code Gen assembly name.
+        /// </summary>
+        /// <returns>
+        /// The Code Gen assembly name.
+        /// </returns>
+        public static string GetCodeGenAssemblyName()
+        {
+            var result = typeof(GenerationShared).Assembly.GetName().Name;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the Code Gen assembly version.
+        /// </summary>
+        /// <returns>
+        /// The Code Gen assembly version.
+        /// </returns>
+        public static string GetCodeGenAssemblyVersion()
+        {
+            var result = typeof(GenerationShared).Assembly.GetName().Version.ToString();
+
+            return result;
+        }
     }
 }
