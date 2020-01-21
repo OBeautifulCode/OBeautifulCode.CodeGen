2aa19767f5b021f39a52d295747ed168868d5c81
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-01-21 15:13:14 -0500

    seed CodeGen.Console

---
+++ OBeautifulCode.CodeGen.Console/.recipes/OBeautifulCode.Bootstrapper/Program.cs
@@ -0,0 +1,51 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.CodeGen.Console
+{
+    using System;
+
+    using CLAP;
+
+    /// <summary>
+    /// Main console entry point class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main console entry point method; downstream is managed by CLAP.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns>Exit code; 0 for success and 1 for error.</returns>
+        public static int Main(string[] args)
+        {
+            try
+            {
+                /*---------------------------------------------------------------------------*
+                 * This is just a pass through to the CLAP implementation of the harness,    *
+                 * it will parse the command line arguments and provide multiple entry       *
+                 * points as configured.                                                     *
+                 *---------------------------------------------------------------------------*/
+                var exitCode = Parser.Run<ConsoleAbstraction>(args);
+
+                return exitCode;
+            }
+            catch (Exception ex)
+            {
+                /*---------------------------------------------------------------------------*
+                 * This should never be reached but is here as a last ditch effort to ensure *
+                 * errors are not lost.                                                      *
+                 *---------------------------------------------------------------------------*/
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(string.Empty);
+
+                return 1;
+            }
+        }
+    }
+}
---
+++ OBeautifulCode.CodeGen.Console/ConsoleAbstraction.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleAbstraction.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.CodeGen.Console
+{
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+
+    using CLAP;
+
+    using OBeautifulCode.Bootstrapper.Recipes;
+    using OBeautifulCode.CodeGen.Console.Internal;
+
+    /// <inheritdoc />
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = ObcSuppressBecause.CA1812_AvoidUninstantiatedInternalClasses_ClassIsWiredIntoClapInProgramCs)]
+    internal class ConsoleAbstraction : ConsoleAbstractionBase
+    {
+        /// <summary>
+        /// Does some work.
+        /// </summary>
+        /// <param name="debug">Optional value indicating whether to launch the debugger from inside the application (default is false).</param>
+        /// <param name="requiredParameter">A required parameter to the operation.</param>
+        [Verb(Aliases = "do", IsDefault = false, Description = "Does some work.")]
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = ObcSuppressBecause.CA1811_AvoidUncalledPrivateCode_MethodIsWiredIntoClapAsVerb)]
+        public static void DoSomeWork(
+            [Aliases("")] [Description("Launches the debugger.")] [DefaultValue(false)] bool debug,
+            [Aliases("")] [Required] [Description("A required parameter to the operation.")] string requiredParameter)
+        {
+            if (debug)
+            {
+                Debugger.Launch();
+            }
+
+            System.Console.WriteLine(requiredParameter);
+        }
+    }
+}
