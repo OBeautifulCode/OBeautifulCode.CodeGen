a1f29846331bd2e48c8fe70010a9ea1004cc5089
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-01-07 12:32:52 -0500

    use latest serialization configuration conventions

--- OBeautifulCode.CodeGen.ModelObject.Test/CodeGeneratorTest/SerializationConfiguration/CodeGenModelObjectTestBsonSerializationConfiguration.cs
+++ OBeautifulCode.CodeGen.ModelObject.Test/CodeGeneratorTest/SerializationConfiguration/CodeGenModelObjectTestBsonSerializationConfiguration.cs
@@ -6,19 +6,35 @@
 
 namespace OBeautifulCode.CodeGen.ModelObject.Test
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     using OBeautifulCode.Serialization.Bson;
+    using OBeautifulCode.Type;
+    using OBeautifulCode.Type.Recipes;
 
     /// <inheritdoc />
     public class CodeGenModelObjectTestBsonSerializationConfiguration : BsonSerializationConfigurationBase
     {
         /// <inheritdoc />
-        protected override IReadOnlyCollection<TypeToRegisterForBson> TypesToRegisterForBson =>
-            CodeGeneratorTestExtensions
-                .GetModelTypes()
-                .Select(_ => _.ToTypeToRegisterForBson())
-                .ToList();
+        protected override IReadOnlyCollection<string> TypeToRegisterNamespacePrefixFilters =>
+            new[]
+            {
+                OBeautifulCode.CodeGen.ModelObject.Test.ProjectInfo.Namespace,
+            };
+
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<BsonSerializationConfigurationType> DependentBsonSerializationConfigurationTypes =>
+            new BsonSerializationConfigurationType[]
+            {
+            };
+
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<TypeToRegisterForBson> TypesToRegisterForBson => new Type[0]
+            .Concat(new[] { typeof(IModel) })
+            .Concat(OBeautifulCode.CodeGen.ModelObject.Test.ProjectInfo.Assembly.GetPublicEnumTypes())
+            .Select(_ => _.ToTypeToRegisterForBson())
+            .ToList();
     }
 }
--- OBeautifulCode.CodeGen.ModelObject.Test/CodeGeneratorTest/SerializationConfiguration/CodeGenModelObjectTestJsonSerializationConfiguration.cs
+++ OBeautifulCode.CodeGen.ModelObject.Test/CodeGeneratorTest/SerializationConfiguration/CodeGenModelObjectTestJsonSerializationConfiguration.cs
@@ -6,19 +6,35 @@
 
 namespace OBeautifulCode.CodeGen.ModelObject.Test
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     using OBeautifulCode.Serialization.Json;
+    using OBeautifulCode.Type;
+    using OBeautifulCode.Type.Recipes;
 
     /// <inheritdoc />
     public class CodeGenModelObjectTestJsonSerializationConfiguration : JsonSerializationConfigurationBase
     {
         /// <inheritdoc />
-        protected override IReadOnlyCollection<TypeToRegisterForJson> TypesToRegisterForJson =>
-            CodeGeneratorTestExtensions
-                .GetModelTypes()
-                .Select(_ => _.ToTypeToRegisterForJson())
-                .ToList();
+        protected override IReadOnlyCollection<string> TypeToRegisterNamespacePrefixFilters =>
+            new[]
+            {
+                OBeautifulCode.CodeGen.ModelObject.Test.ProjectInfo.Namespace,
+            };
+
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<JsonSerializationConfigurationType> DependentJsonSerializationConfigurationTypes =>
+            new JsonSerializationConfigurationType[]
+            {
+            };
+
+        /// <inheritdoc />
+        protected override IReadOnlyCollection<TypeToRegisterForJson> TypesToRegisterForJson => new Type[0]
+            .Concat(new[] { typeof(IModel) })
+            .Concat(OBeautifulCode.CodeGen.ModelObject.Test.ProjectInfo.Assembly.GetPublicEnumTypes())
+            .Select(_ => _.ToTypeToRegisterForJson())
+            .ToList();
     }
 }
